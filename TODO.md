eedb7ec80cab6b947437d0b972121b622e22cd3b
Shawn Taylor <shawn@mapsandapps.net>
2021-09-29 07:52:13 -0400

    Convert 8-digit hex code to rgba using input box and information message

---
+++ TODO.md
@@ -0,0 +1,6 @@
+* keep watching the video https://www.youtube.com/watch?v=q5V4T3o3CXE&list=WL&index=2
+* convert rgba array to string
+* add a setting for string format
+* add an input box in extension - to get hex from user
+* let them copy the code that's returned
+* perhaps instead of taking an input, it should use the selected text
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as chroma from 'chroma-js'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "color-code-converter" is now active!');
+  console.debug(
+    'Congratulations, your extension "color-code-converter" is now active!'
+  )
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('color-code-converter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Color Code Converter!');
-	});
+  // The command has been defined in the package.json file
+  // The commandId parameter must match the command field in package.json
+  let convert8DigitToRgba = vscode.commands.registerCommand(
+    'color-code-converter.convert8DigitToRgba',
+    async () => {
+      let colorInput: string | undefined = await vscode.window.showInputBox({
+        placeHolder: 'Color code'
+      })
 
-	context.subscriptions.push(disposable);
+      if (!colorInput) return // TODO
+
+      vscode.window.showInformationMessage(chroma(colorInput).css())
+    }
+  )
+
+  context.subscriptions.push(convert8DigitToRgba)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
