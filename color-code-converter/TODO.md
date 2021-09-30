53361d985a1b55b8b7eb27c8862230ac4e8d4cab
Shawn Taylor <shawn@mapsandapps.net>
2021-09-29 22:56:36 -0400

    Allow user to select desired format

--- color-code-converter/TODO.md
+++ color-code-converter/TODO.md
@@ -1,7 +1,5 @@
 * keep watching the video https://www.youtube.com/watch?v=q5V4T3o3CXE&list=WL&index=2
-* add a setting for string format
-* let them copy the code that's returned
-* perhaps instead of taking an input, it should use the selected text
+* add a setting for string format (e.g. rgb(0,0,0) vs rgb(0, 0, 0))
 * write tests
 * what's the difference between registerCommand and registerTextEditorCommand?
 
@@ -9,3 +7,5 @@
 DONE
 * convert rgba array to string
 * add an input box in extension - to get hex from user
+* perhaps instead of taking an input, it should use the selected text
+* perhaps have the command palette just be "convert color" and then once that's selected, have a menu pop up with each option to convert to and display a preview of what the first selected item will change to (although this requires more typing from user, so might not be as good?)
--- color-code-converter/src/extension.ts
+++ color-code-converter/src/extension.ts
@@ -1,22 +1,75 @@
 import * as vscode from 'vscode'
 import * as chroma from 'chroma-js'
 
+interface ColorFormat {
+  transform: (color: string) => string
+  label: string
+}
+
+const formats: ColorFormat[] = [
+  {
+    transform: (color: string) => {
+      return chroma(color).hex()
+    },
+    label: 'hex'
+  },
+  {
+    transform: (color: string) => {
+      return chroma(color).css()
+    },
+    label: 'rgb'
+  }
+]
+
+const convert8DigitToRgba = vscode.commands.registerTextEditorCommand(
+  'color-code-converter.convert8DigitToRgba',
+  (textEditor: vscode.TextEditor) => {
+    let selections = textEditor.selections.map(
+      (s) => new vscode.Range(s.start, s.end)
+    )
+
+    // TODO: do something if there are no selections: allow user input?
+
+    const firstSelection = textEditor.document.getText(selections[0])
+
+    const quickPickItems: vscode.QuickPickItem[] = formats.map((format) => {
+      return {
+        description: format.transform(firstSelection),
+        label: format.label
+      }
+    })
+
+    const onDidSelectItem = (value: vscode.QuickPickItem) => {
+      const transformFunction = formats.find(
+        ({ label }) => label === value.label
+      )?.transform
+
+      if (transformFunction) {
+        // earlier edit no longer valid; start a new edit
+        textEditor.edit((edit) => {
+          for (const selection of textEditor.selections) {
+            if (selection.isEmpty) {
+              // TODO
+            } else {
+              const selectionText = textEditor.document.getText(selection)
+              edit.replace(selection, transformFunction(selectionText))
+            }
+          }
+        })
+      } // TODO: else
+    }
+
+    vscode.window.showQuickPick(quickPickItems).then((selectedFormat) => {
+      if (selectedFormat) {
+        onDidSelectItem(selectedFormat)
+      }
+    })
+  }
+)
+
 export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // The commandId parameter must match the command field in package.json
-  const convert8DigitToRgba = vscode.commands.registerTextEditorCommand(
-    'color-code-converter.convert8DigitToRgba',
-    (editor, editBuilder) => {
-      let selections = editor.selections.map(
-        (s) => new vscode.Range(s.start, s.end)
-      )
-
-      selections.map((selection) => {
-        const selectionText = editor.document.getText(selection)
-        editBuilder.replace(selection, transform(selectionText))
-      })
-    }
-  )
 
   context.subscriptions.push(convert8DigitToRgba)
 }
